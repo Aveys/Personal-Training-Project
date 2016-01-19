e262e24f20b6831c2ad3cc730fe9a70ba0fa69c7
arca223 <arcanis223@gmail.com>
2016-01-19 16:59:11 +0100

    Ajout exercice en graphique

---
+++ src/servlet/AddTrainingServlet.java
@@ -0,0 +1,19 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Max TOMPOUCE on 19/01/2016.
+ */
+public class AddTrainingServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //super.doPost(req, resp);
+
+    }
+}
---
+++ web/js/addTrainingAJAX.js
@@ -0,0 +1,53 @@
+/**
+ * Created by Max TOMPOUCE on 19/01/2016.
+ */
+
+
+
+$('#btn-exercise').click(function() {
+    var data={};
+    data.type="exercise";
+    data.title=$('#titleDescription').val();
+    data.desc=$('#exerciseDescription').val();
+    data.mn=$('#ex-time-mn').val();
+    data.s=$('#ex-time-s').val();
+    data.row=$('#ex-row').val();
+    $.ajax
+    ({
+        url: '/addTraining',
+        data: data,
+        type: 'post',
+        success: function(result)
+        {
+            var row = $('<tr>');
+            row.append($('<td>').html(data.title));
+            row.append($('<td>').html(data.desc));
+            row.append($('<td>').html(data.mn+':'+data.s));
+            row.append($('<td>').html(data.row));
+            row.append($('<td> <button type="submit" class="btn btn-danger btn-sm"> <span class="glyphicon glyphicon-remove"></span> </button></td>'));
+
+            $('#row-exercise').append(row);
+
+            //TODO
+            //change of total time
+            //$('#totalTimeValue').text()
+        }
+    });
+});
+
+
+
+$('#trainingSubmit').click(function() {
+    var data={};
+    data.type="plan";
+    $.ajax()
+    ({
+        url: '/addTraining',
+        data: data,
+        type: 'post',
+        success: function(result)
+        {
+
+        }
+    });
+});
