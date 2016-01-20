75dc608cec292f18b0c19afa629b49dae72901c1
Arthur Veys <veysarthur@gmail.com>
2016-01-20 13:04:55 +0100

    auth fonctionnelle à 100%

---
+++ web/js/navbarAjax.js
@@ -0,0 +1,31 @@
+/**
+ * Created by arthurveys on 20/01/16.
+ */
+
+var id_token;
+function onSignIn(googleUser) {
+    // Useful data for your client-side scripts:
+    id_token = googleUser.getAuthResponse().id_token;
+    /*var profile = googleUser.getBasicProfile();
+     console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+     console.log("Name: " + profile.getName());
+     console.log("Image URL: " + profile.getImageUrl());
+     console.log("Email: " + profile.getEmail());*/
+    data = {};
+    data.tokenID=id_token;
+    $.ajax
+    ({
+        url: '/tokenVerifier',
+        data: data,
+        type: 'post',
+        success: function(result)
+        {
+            $('#google-button').hide();
+            $('#navbar-list').append("<li><a href=\" \">Welcome "+result.name+"<br/>"+result.mail+"</a></li>");
+        },
+        error: function () {
+            alert("Token Invalid");
+        }
+    });
+
+}
